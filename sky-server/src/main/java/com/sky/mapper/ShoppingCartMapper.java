606b1754c5c6e410c77ccc90d3189884c83aa4de
tiantianxie <1164567053@qq.com>
2023-10-04 11:03:33 +0800

    业务开发--下单

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -8,16 +8,16 @@ import java.util.List;
 @Mapper
 public interface ShoppingCartMapper {
     List<ShoppingCart> list(ShoppingCart shoppingCart);
-    @Update("update shopping_cart set number = #{number} where id = #{id} ")
+    @Update("update sky_take_out.shopping_cart set number = #{number} where id = #{id} ")
     void updateNumber(ShoppingCart shoppingCart);
-    @Insert("insert into shopping_cart (name, user_id, dish_id, setmeal_id, dish_flavor, number, amount, image, create_time) " +
+    @Insert("insert into sky_take_out.shopping_cart (name, user_id, dish_id, setmeal_id, dish_flavor, number, amount, image, create_time) " +
             " values (#{name},#{userId},#{dishId},#{setmealId},#{dishFlavor},#{number},#{amount},#{image},#{createTime})")
     void insert(ShoppingCart shoppingCart);
-    @Delete("delete from shopping_cart where user_id = #{userId}")
+    @Delete("delete from sky_take_out.shopping_cart where user_id = #{userId}")
     void cleanAll(ShoppingCart build);
 
     void deleteOne(ShoppingCart shoppingCart);
     ShoppingCart query(ShoppingCart shoppingCart);
-    @Delete("delete from shopping_cart where user_id = #{userId}")
+    @Delete("delete from sky_take_out.shopping_cart where user_id = #{userId}")
     void deleteByUserId(Long userId);
 }
