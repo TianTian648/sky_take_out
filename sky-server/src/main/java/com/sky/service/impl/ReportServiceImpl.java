41e366b7d017c13bff4df5a7159ba85a2a4854fb
tiantianxie <1164567053@qq.com>
2023-10-05 16:46:36 +0800

    业务开发--营业额统计

---
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -0,0 +1,62 @@
+package com.sky.service.impl;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.ReportMapper;
+import com.sky.service.ReportService;
+import com.sky.vo.TurnoverReportVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Slf4j
+public class ReportServiceImpl implements ReportService {
+    @Autowired
+    private ReportMapper reportMapper;
+
+    /**
+     * 根据时间统计营业额
+     *
+     * @param begin
+     * @param end
+     * @return
+     */
+    @Override
+    public TurnoverReportVO turnoverStatistics(LocalDate begin, LocalDate end) {
+        //select count(amount) from orders where order_time > begin and order_time < end and status = 5
+        List<LocalDate> localDateList = new ArrayList<>();
+        localDateList.add(begin);
+        log.info("begin:{}",begin);
+        log.info("end:{}",end);
+        while (!begin.equals(end)){
+            begin = begin.plusDays(1);//日期计算，获得指定日期后1天的日期
+            localDateList.add(begin);
+        }
+
+        List<Double> turnoverList = new ArrayList<>();
+        for (LocalDate localDate : localDateList) {
+            LocalDateTime beginTime = LocalDateTime.of(localDate, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(localDate, LocalTime.MAX);
+            Map map = new HashMap<>();
+            map.put("status", Orders.COMPLETED);
+            map.put("begin", beginTime);
+            map.put("end", endTime);
+            Double turnover = reportMapper.turnoverStatistics(map);
+            log.info("turnover:{}", turnover);
+            turnover = turnover == null ? 0.0 : turnover;
+            turnoverList.add(turnover);
+        }
+         return  TurnoverReportVO.builder()
+                 .dateList(localDateList.toString())
+                 .turnoverList(turnoverList.toString())
+                 .build();
+    }
+}
